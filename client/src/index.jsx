16a605b24dd20d855a884262e7f64eea83817a62
AmagiDDmxh <amagiddmxh@gmail.com>
2022-02-09 00:27:16 +0800

    Use custom provider for Aurora chain

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider as WAGMIProvider } from 'wagmi'
+import { providers } from 'ethers'
 
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { connectors } from 'utils/connectors'
+
+// eslint-disable-next-line semi
+const providerUrl = import.meta.env.PROD ? 'https://mainnet.aurora.dev' : 'https://testnet.aurora.dev'
+const provider = () => new providers.JsonRpcProvider(providerUrl)
 
 ReactDOM.render(
-  <WAGMIProvider>
+  <WAGMIProvider connectors={connectors} provider={provider} autoConnect>
     <App />
   </WAGMIProvider>,
   document.getElementById('root')
