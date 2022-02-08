dbcd020e701a950d36f387f7e23dbad7bd779445
AmagiDDmxh <amagiddmxh@gmail.com>
2022-02-09 00:27:29 +0800

    Add simple contract hook

--- client/src/utils/contracts.ts
+++ client/src/utils/contracts.ts
@@ -1,10 +1,16 @@
-import ethers from 'ethers'
+import { useContract, useProvider } from 'wagmi'
 import CErc20Delegator from '../contracts/CToken/CErc20Delegator.sol/CErc20Delegator.json'
 
-const provider = new ethers.providers.Web3Provider((window as any).ethereum)
-
 const cErc20DelegatorAddress = '0x70A71b86594EabB041c92f6D983c23c77848c868'
-export const getCTokenContract = async (address: string) => {
-	const signer = provider.getSigner(address)
-	return new ethers.Contract(cErc20DelegatorAddress, CErc20Delegator.abi, signer)
+// export const getCTokenContract = async (address: string) => {
+//   const signer = provider.getSigner(address)
+//   return new ethers.Contract(cErc20DelegatorAddress, CErc20Delegator.abi, signer)
+// }
+export const useCTokenContract = () => {
+  const provider = useProvider()
+  return useContract({
+    addressOrName: cErc20DelegatorAddress,
+    contractInterface: CErc20Delegator.abi,
+    signerOrProvider: provider,
+  })
 }
