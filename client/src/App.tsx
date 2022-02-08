2c788f510597c7549f7bbef062251b0998c75223
AmagiDDmxh <amagiddmxh@gmail.com>
2022-02-09 00:28:05 +0800

    Update to functional comp, add contract usage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 import OnePage from './components/One/one'
@@ -20,13 +20,12 @@ import Logo from './assets/Logo.png'
 import { Modal, Tabs, Image } from 'antd'
 
 //CToken合约
-import { borrowBalanceStored, exchangeRateCurrent, exchangeRateStored, mint } from './utils/CErc20Delegator/chain'
 import { HeaderActions } from 'components/HeaderActions'
 
 const { TabPane } = Tabs
 
-class App extends Component {
-  state = {
+export const App = () => {
+  const [state, setState] = useState({
     loading: false,
     storageValue: 0,
     accounts: null,
@@ -34,158 +33,127 @@ class App extends Component {
     showmodalc: false,
     showmodalp: false,
     showmodalmarket: false,
+  })
+
+  const handleCancel = () => {
+    setState((s) => ({ ...s, showmodalc: false }))
   }
 
-  handleCancel = () => {
-    this.setState({
-      showmodalc: false,
-    })
+  const handleCancelP = () => {
+    setState((s) => ({ ...s, showmodalp: false }))
   }
 
-  handleCancelP = () => {
-    this.setState({
-      showmodalp: false,
-    })
+  const handleCancelMarket = () => {
+    setState((s) => ({ ...s, showmodalmarket: false }))
   }
 
-  handleCancelMarket = () => {
-    this.setState({
-      showmodalmarket: false,
-    })
+  const showModalC = () => {
+    setState((s) => ({ ...s, showmodalc: true }))
+    console.log(state.showmodalc)
   }
 
-  showModalC = () => {
-    this.setState({
-      showmodalc: true,
-    })
-    console.log(this.state.showmodalc)
+  const showModalP = () => {
+    setState((s) => ({ ...s, showmodalp: true }))
   }
 
-  showModalP = () => {
-    this.setState({
-      showmodalp: true,
-    })
+  const showModalMarket = () => {
+    setState((s) => ({ ...s, showmodalmarket: true }))
   }
 
-  showModalMarket = () => {
-    this.setState({
-      showmodalmarket: true,
-    })
-  }
-
-  connectWallet = async () => {
-    // console.log("connect web3");
-    // if (typeof window.ethereum !== 'undefined') {
-    //   console.log('MetaMask is installed!');
-    // }else{
-    //   window.alert('Please install MetaMask first.');
-    //   return;
-    // }
-
-    // //链接metamask钱包
-    // window.ethereum.request({ method: 'eth_requestAccounts' });
-    // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    // const account = accounts[0];
-    // console.log(account);
-
-    // -------------------------测试CToken接口------------------------
-    // -------------------------测试CToken接口------------------------
-    await borrowBalanceStored() // OK
-    // await exchangeRateCurrent();// fail
-    // await exchangeRateStored();// OK
-    // await mint(0);// fail
-    this.setState({ loading: true }) //到这里metamask就连接上了，状态为true
-  }
-
-  OperationsSlot = {
+  const OperationsSlot = {
     left: <Image preview={false} className="lc" src={Logo} />,
-    right: (
-      <HeaderActions />
-    ),
+    right: <HeaderActions />,
   }
 
-  render() {
-    return (
-      <div>
-        <div className="App">
-          <Tabs className="tabs" defaultActiveKey="1" tabBarExtraContent={this.OperationsSlot}>
-            <TabPane className="tabs_c" tab="流动性" key="1">
-              <OnePage SetShowModalC={this.showModalC} SetShowModalP={this.showModalP} />
+  // Contract usage example
+  // const [{ data }] = useAccount()
+  // const cTokenContarct = useCTokenContract()
+  // useEffect(() => {
+  //   if (!data?.address) {
+  //     return
+  //   }
+
+  //   ;(async () => {
+  //     console.log('contract')
+  //     console.log(cTokenContarct)
+  //     console.log(await cTokenContarct.balanceOf(data.address))
+  //   })()
+  // }, [data])
+
+  return (
+    <div>
+      <div className="App">
+        <Tabs className="tabs" defaultActiveKey="1" tabBarExtraContent={OperationsSlot}>
+          <TabPane className="tabs_c" tab="流动性" key="1">
+            <OnePage SetShowModalC={showModalC} SetShowModalP={showModalP} />
+          </TabPane>
+          <TabPane className="tabs_c" tab="借贷+稳定币" key="2">
+            <TwoPage SetShowModalMarket={showModalMarket} />
+          </TabPane>
+        </Tabs>
+      </div>
+
+      {/* Lock Modal */}
+      <Modal className="modal_c" visible={state.showmodalc} title="Lock And Vote" onCancel={handleCancel} footer={[]}>
+        <div>
+          <Tabs className="modalctabnav" defaultActiveKey="1" centered>
+            <TabPane tab="LOCK" key="1">
+              <ModalLock />
             </TabPane>
-            <TabPane className="tabs_c" tab="借贷+稳定币" key="2">
-              <TwoPage SetShowModalMarket={this.showModalMarket} />
+            <TabPane tab="WITHDRAW" key="2">
+              <ModalWithdraw />
             </TabPane>
           </Tabs>
         </div>
+      </Modal>
 
-        {/* Lock Modal */}
-        <Modal
-          className="modal_c"
-          visible={this.state.showmodalc}
-          title="Lock And Vote"
-          onCancel={this.handleCancel}
-          footer={[]}
-        >
-          <div>
-            <Tabs className="modalctabnav" defaultActiveKey="1" centered>
-              <TabPane tab="LOCK" key="1">
-                <ModalLock />
-              </TabPane>
-              <TabPane tab="WITHDRAW" key="2">
-                <ModalWithdraw />
-              </TabPane>
-            </Tabs>
-          </div>
-        </Modal>
+      {/* Provide Modal */}
+      <Modal
+        className="modal_p"
+        visible={state.showmodalp}
+        title="Provide Liquidity"
+        onCancel={handleCancelP}
+        footer={[]}
+      >
+        <div>
+          <Tabs className="modalptabnav" defaultActiveKey="1" centered>
+            <TabPane tab="DEPOSIT" key="1">
+              <ModalPDeposit />
+            </TabPane>
+            <TabPane tab="WITHDRAW" key="2">
+              <ModalPWithdraw />
+            </TabPane>
+          </Tabs>
+        </div>
+      </Modal>
 
-        {/* Provide Modal */}
-        <Modal
-          className="modal_p"
-          visible={this.state.showmodalp}
-          title="Provide Liquidity"
-          onCancel={this.handleCancelP}
-          footer={[]}
-        >
-          <div>
-            <Tabs className="modalptabnav" defaultActiveKey="1" centered>
-              <TabPane tab="DEPOSIT" key="1">
-                <ModalPDeposit />
-              </TabPane>
-              <TabPane tab="WITHDRAW" key="2">
-                <ModalPWithdraw />
-              </TabPane>
-            </Tabs>
-          </div>
-        </Modal>
-
-        {/* Lock Modal Market*/}
-        <Modal
-          className="modal_market"
-          visible={this.state.showmodalmarket}
-          title=""
-          onCancel={this.handleCancelMarket}
-          footer={[]}
-        >
-          <div>
-            <Tabs className="modalmarkettabnav" defaultActiveKey="1" centered>
-              <TabPane tab="SUPPLY" key="1">
-                <ModalMarketSupply />
-              </TabPane>
-              <TabPane tab="BORROW" key="2">
-                <ModalMarketBorrow />
-              </TabPane>
-              <TabPane tab="WITHDRAW" key="3">
-                <ModalMarketWithdraw />
-              </TabPane>
-              <TabPane tab="REPAY" key="4">
-                <ModalMarketRepay />
-              </TabPane>
-            </Tabs>
-          </div>
-        </Modal>
-      </div>
-    )
-  }
+      {/* Lock Modal Market*/}
+      <Modal
+        className="modal_market"
+        visible={state.showmodalmarket}
+        title=""
+        onCancel={handleCancelMarket}
+        footer={[]}
+      >
+        <div>
+          <Tabs className="modalmarkettabnav" defaultActiveKey="1" centered>
+            <TabPane tab="SUPPLY" key="1">
+              <ModalMarketSupply />
+            </TabPane>
+            <TabPane tab="BORROW" key="2">
+              <ModalMarketBorrow />
+            </TabPane>
+            <TabPane tab="WITHDRAW" key="3">
+              <ModalMarketWithdraw />
+            </TabPane>
+            <TabPane tab="REPAY" key="4">
+              <ModalMarketRepay />
+            </TabPane>
+          </Tabs>
+        </div>
+      </Modal>
+    </div>
+  )
 }
 
 export default App
